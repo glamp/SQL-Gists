1c5a47d9291cc47ac0712f0a3a9e94999552057a
Greg <greg@yhatHQ.com>
2013-12-30 22:22:51 -0500

    le gist

--- second-query.sql
+++ second-query.sql
@@ -1,3 +1,6 @@
+days = (select distinct created_date as ith_day from users)
+--OR--
+days = ('2013-01-01', '2013-06-01', '2013-12-01')
 
 {{#days}}
 select
@@ -7,3 +10,14 @@ select
 from
     foo;
 {{/days}}
+
+stuffs = (select distinct name as human_name, created_date as ith_day from users)
+
+{{#stuffs}}
+select
+    {{ human_name }}
+    , {{ ith_day }}
+    , address
+from
+    foo;
+{{/stuffs}}
