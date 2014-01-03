3c34097f6e4ea0307c719a387ebdd03184bf3d32
Greg <greg@yhatHQ.com>
2014-01-03 10:29:54 -0700

    le gist

--- second-query.sql
+++ second-query.sql
@@ -1,6 +1,6 @@
 days = { 'days': ['2013-01-01', '2013-06-01', '2013-12-01'] }
-days = (select distinct created_date as ith_day from sample_days)
 --OR--
+days = (select distinct created_date as ith_day from sample_days)
 
 {{#days}}
 select
