7ca18d0e6a9e21ff5c339d814cadedc9885b810b
Greg <greg@yhatHQ.com>
2014-01-03 15:00:05 -0700

    le gist

--- second-query.sql
+++ second-query.sql
@@ -1,18 +1,12 @@
-days = {
-    'days': [
-        {'ith_day': '2013-01-01'},
-        {'ith_day': '2013-06-01'},
-        {'ith_day': '2013-12-01'}
-    ]
-}
+
 --OR--
 days = (select distinct created_date as ith_day from sample_days)
-
+days = { 'days': [ {'ith_day': '2013-01-01'}, {'ith_day': '2013-06-01'}, {'ith_day': '2013-12-01'} ]}
 {{#days}}
 select
     name
     , address
-    , {{ ith_day }} as day_n
+    , '{{ ith_day }}' as day_n
 from
     foo;
 {{/days}}
